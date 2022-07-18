5ffea6034c8bc4f722eb1808f6ab8da6f2f17b76
Diego I Cea <64449589+DicesaDev@users.noreply.github.com>
2022-07-18 17:57:21 -0400

    Update QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -1,6 +1,6 @@
 import pyqrcode
 from pyqrcode import QRCode
 
-url = pyqrcode.create('https://www.linkedin.com/in/diceasa/')
+url = pyqrcode.create('https://www.tuweb.cl/')
 url.svg('MiQR.svg', scale=8)
 print(url)
